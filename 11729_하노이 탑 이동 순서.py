a334f4cc59856cb274519f5b20a857467882a2c9
Park  Dong-jun <70496139+Lairin-pdj@users.noreply.github.com>
2021-09-29 04:46:49 +0900

    Update 11729_하노이 탑 이동 순서.py

--- 11729_하노이 탑 이동 순서.py
+++ 11729_하노이 탑 이동 순서.py
@@ -1,24 +1,11 @@
 def hanoi(start, temp, end, n):
-    global lines
-    
     if n == 1:
-        lines.append([start, end])
-        
-        return 1
+        print(start, end)
     else:
-        count = 0
-        count += hanoi(start, end, temp, n - 1)
-        count += hanoi(start, temp, end, 1) 
-        count += hanoi(temp, start, end, n - 1)
-        
-        return count
-    
+        hanoi(start, end, temp, n - 1)
+        print(start, end)
+        hanoi(temp, start, end, n - 1)
 
 n = int(input())
-
-global lines
-lines = []
-
-print(hanoi(1, 2, 3, n))
-for a, b in lines:
-    print(a, b)
+print((2 ** n) - 1)
+hanoi(1, 2, 3, n)
