f9aadb1ab1b942ab767def98aaf136390782cbd8
Park  Dong-jun <70496139+Lairin-pdj@users.noreply.github.com>
2021-09-30 17:10:25 +0900

    Create 15649_N과 M (1).py

---
+++ 15649_N과 M (1).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def track(n, count, route):
+    if count == 0:
+        print(" ".join(map(str, route)))
+    else:
+        for i in range(1, n + 1):
+            if i not in route:
+                track(n, count - 1, route + [i])
+
+n, count = map(int, input().split(" "))
+
+track(n, count, [])
