98e2abd7cdf246c0b27ebf4fab0f3c25dbff6cba
Park  Dong-jun <70496139+Lairin-pdj@users.noreply.github.com>
2021-10-03 22:48:44 +0900

    Update 1904_01타일.py

--- 1904_01타일.py
+++ 1904_01타일.py
@@ -3,11 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [0] * (n + 1)
-dp[1] = 1
-dp[2] = 2
+if n == 1:
+    print(1)
+else:
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
 
-for i in range(3, n + 1):
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746    
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746    
 
-print(dp[n])
+    print(dp[n])
